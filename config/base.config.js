ab6e08bc412dd85d2348e5fa445c0c1078d6d3d4
Huliya <1192163814@qq.com>
2021-03-31 15:14:39 +0800

    feat:babel-loader

--- config/base.config.js
+++ config/base.config.js
@@ -15,6 +15,20 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.m?js$/i,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                '@babel/preset-env'
+              ]
+            }
+          }
+        ]
+      },
+      {
         test: /\.css$/i,
         use: [
           // 提取css文件
@@ -45,14 +59,14 @@ module.exports = {
           // 源路径
           from: "./public",
           // 目标路径
-          to: path.resolve(__dirname,"../build"),
-          globOptions:{
+          to: path.resolve(__dirname, "../build"),
+          globOptions: {
             // index.html已经由html-webpack-plugin生成，再次复制会报错，所以这里忽略index.html不进行复制
-            ignore:["**/*index.html"]
+            ignore: ["**/*index.html"]
           }
         }
       ],
-      options:{
+      options: {
         // fs文件操作的并发数
         concurrency: 100,
       }
